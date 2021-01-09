96722b1e7d1652d58483173d3c659d17002dc2a3
git-sravya <s.sravya@yahoo.com>
2021-01-09 12:11:38 +0530

    Add project files.

---
+++ Sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Some more code");
+            Console.Read();
+        }
+    }
+}
